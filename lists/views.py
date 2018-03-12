1f3e3e9807c113729b1d0b45db3f439bf9f24322
Dex Neo <dex.neo@gmx.com>
2018-03-12 17:06:18 +0100

    Refactored home page view to use template. Started using the Django Test Client.

--- lists/views.py
+++ lists/views.py
@@ -1,7 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 
 def home_page(request):
-    minimal_page = '<html><head><title>To-Do lists</title></head></html>'
-    return HttpResponse(minimal_page)
+    return render(request, 'home.html')
