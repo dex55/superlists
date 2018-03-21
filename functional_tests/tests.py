2750e888b5bbf2503313c4769c2bfd980a5b9980
Dex Neo <dex.neo@gmx.com>
2018-03-21 02:06:41 +0100

    First steps of FT for layout and styling.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -2,6 +2,8 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 from django.test import LiveServerTestCase
+
+
 from time import sleep, time
 
 
@@ -130,3 +132,31 @@ class NewVisitorTest(LiveServerTestCase):
         self.assertNotIn('Buy milk', page_text)
 
         # Satisfied, they both go back to sleep
+
+    def test_layout_and_styling(self):
+
+        # Edith goes to the home page
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # She notices that the input box is nicely centered
+        inputbox = self.browser.find_element_by_id('new_item_input')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # She starts a new list...
+        inputbox.send_keys('testing')
+        inputbox.send_keys(Keys.ENTER)
+
+        self.wait_for_row_in_list_table('1: testing')
+
+        # ... and sees that the input is nicely centered there as well.
+        inputbox = self.browser.find_element_by_id('new_item_input')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
