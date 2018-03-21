7bec7e8613969ee94b855df17eff2dd711aa85ad
Dex Neo <dex.neo@gmx.com>
2018-03-21 04:15:27 +0100

    Started using Bootstrap CSS framework to improve layout. Added some static file support.

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
-from django.test import LiveServerTestCase
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 
 
 from time import sleep, time
@@ -10,7 +10,7 @@ from time import sleep, time
 MAX_WAIT = 5
 
 
-class NewVisitorTest(LiveServerTestCase):
+class NewVisitorTest(StaticLiveServerTestCase):
 
     def setUp(self):
         self.browser = webdriver.Firefox()
